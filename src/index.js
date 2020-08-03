e59d2766f0b54fc31bac688a5f4ac48be888c9bd
Anthony San Miguel <asanmiguel1088@gmail.com>
2020-08-02 19:05:55 -0500

    completed React tictactoe game initial tutorial

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+// TO RUN PROGRAM: Type 'npm start' in Terminal
+
+
 // Renders React component 'Square' = which is a single <button>
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick({value: 'X'})} // When an individual square is 'clicked' on by the user, its 'state' value will change to 'X' and display as such
-            >
-                {this.props.value} {/* When 'Square' method is called, each rendered square (e.g. <button>) will have a 'state' value, which will change to 'X' on click */}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>{props.value}</button>
+    );
 }
 
 // Renders React component 'Board' = which is 9 squares
@@ -23,7 +20,21 @@ class Board extends React.Component {
         super(props); // *NOTE: Need to always call 'super()' when defining the constructor of a subclass...all React component classes that have a constructor should start with a 'super(props)' call
         this.state = {
             squares: Array(9).fill(null), // On page load, the Board's initial state will contain 9 nulls (one for each square rendered)
+            xIsNext: true, // Sets first move, or 'click', to be 'X' by default, rather than 'O' (second player)
         };
+    }
+
+    // Function to run on square 'click'; Board will loop through the rendered squares and change the 'null' state of the clicked on squares to 'X'
+    handleClick(i){
+        const squares = this.state.squares.slice(); // '.slice' creates a copy of the squares array to modify, rather than change existing one
+        if (calculateWinner(squares) || squares[i]) { // If a winner has already been declared, or the current square has already been clicked, don't do anything on click
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O'; // Ternary statement that determines a square's 'state' ('X' or 'O') on a given turn...
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext, // Flips the boolean 'xIsNext' so that every other turn, or 'click' is for player 2 ('O') instead of 'X'
+        });
     }
 
     renderSquare(i) {
@@ -35,7 +46,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -77,6 +94,26 @@ class Game extends React.Component {
     }
 }
 
+function calculateWinner(squares) {
+    const lines = [
+        // All ways to win (horizontally, diagonally, etc.)
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 // ========================================
 
 ReactDOM.render(
